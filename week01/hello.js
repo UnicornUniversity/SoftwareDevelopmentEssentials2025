d40b61869ce6a087f8b62ee39b318bfbe8160cac
GarmoskaDobra <leonov.eugene@gmail.com>
2025-09-29 11:31:42 +0200

    week 01 completed

---
+++ week01/hello.js
@@ -0,0 +1,30 @@
+// this is my code
+console.log("Hello World!!!");
+var x;
+x = 200;
+console.log("x = " + x);
+x = 300;
+console.log("x = " + x);
+x = "don't do this";
+console.log("x = " + x);
+
+let y = 100;
+let z = "hello!";
+
+const a = 900;
+console.log("a = " + a);
+//a = 1000;
+
+// math
+let b = 2 + 2;
+console.log("b = " + b);
+let c;
+console.log("c = " + c);
+c = b;
+console.log("c = " + c);
+
+
+
+
+
+
